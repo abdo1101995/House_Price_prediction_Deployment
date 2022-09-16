c6026292a9b1638af0110a48ac08d0fb5fe85895
unknown <abomahmud7211@gmail.com>
2022-09-16 16:22:04 +0200

    ml pro

---
+++ router.py
@@ -0,0 +1,57 @@
+
+from distutils.log import debug
+import numpy as np
+import pandas as pd
+import joblib
+from distutils.log import debug
+from flask import Flask ,render_template,request
+from utils import process_new
+
+model=joblib.load('model_xgb.pkl')
+#intialize
+
+app=Flask(__name__)
+
+
+
+@app.route('/home')
+
+def home():
+    return  render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+
+def predict():
+    if request.method=='POST':
+        long=float(request.form['long'])
+        lat=float(request.form['lati'])
+        house_median=float(request.form['h_med'])
+        total_rooms=float(request.form['to_ro'])
+        total_bedrooms=float(request.form['to_bro'])
+        pop=float(request.form['pop'])
+        household=float(request.form['houssh'])
+        median_income=float(request.form['med'])
+        ocean_proximity=request.form['ocean']
+        rooms_per_holds=total_rooms/household
+        bedrooms_per_rooms=total_bedrooms/total_rooms
+        pop_per_hold=pop/household
+        X_new=pd.DataFrame({'longitude':[long],'latitude':[lat],'housing_median_age':[house_median],'total_rooms':[total_rooms],
+        'total_bedrooms':[total_bedrooms],'population':[pop],'households':[household],'median_income':[median_income],
+        'ocean_proximity':[ocean_proximity],'room_per_houseshold':[rooms_per_holds],'bedromms_per_rooms':[bedrooms_per_rooms],
+        'population_per_houseshold':[pop_per_hold]})
+
+        X_processed=process_new(X_new)
+        y_pred_new=model.predict(X_processed)
+        y_pred_new='{:0.4f}'.format(y_pred_new[0])
+        return  render_template('predict.html',pred_vals=y_pred_new)
+    else:
+        return  render_template('predict.html')
+
+@app.route('/about')
+
+def about():
+    return  render_template('about.html')
+
+#terminal
+if __name__=='__main__':
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline ,FeatureUnion
+from sklearn_features.transformers import DataFrameSelector
+from sklearn.preprocessing import StandardScaler ,OneHotEncoder
+
+
+# In[2]:
+
+
+#file path to read it
+FILE_PATH='D:\house_deployment\housing.csv'
+# read file by pandas 
+df=pd.read_csv(FILE_PATH)
+#replace '<1H OCEAN' to 'OCEAN'
+df['ocean_proximity']=df['ocean_proximity'].replace('<1H OCEAN','OCEAN')
+#add more columns may be useful for model
+df['room_per_houseshold']=df['total_rooms']/df['households']
+df['bedromms_per_rooms']=df['total_bedrooms']/df['total_rooms']
+df['population_per_houseshold']=df['population']/df['households']
+X=df.drop(columns='median_house_value')
+Y=df['median_house_value']
+#split data to train ,test
+X_train,X_test,y_train,y_test=train_test_split(X,Y,shuffle=True ,test_size=0.2,random_state=42)
+num_cols=[col for col in X_train.columns if X_train[col].dtype in ['int32','int64','float32','float64'] ]
+cat_cols=list(set(X_train.columns)-set(num_cols))
+
+
+# In[3]:
+
+
+num_pipeline=Pipeline(steps=[
+    ('selector',DataFrameSelector(num_cols)),
+    ('imputer',SimpleImputer(strategy='median')),
+    ('scalar',StandardScaler())
+])
+pipeline_cat=Pipeline(steps=[
+    ('selector',DataFrameSelector(cat_cols)),
+    ('imputer',SimpleImputer(strategy='constant',fill_value='missing')),
+    ('ohe',OneHotEncoder(sparse=False))
+])
+total_pipeline=FeatureUnion(transformer_list=[
+    ('num',num_pipeline),
+    ('cat',pipeline_cat)
+])
+X_train_final=total_pipeline.fit_transform(X_train)
+
+
+# In[4]:
+
+
+def process_new(X_new):
+    return total_pipeline.transform(X_new)
+
+
+# In[ ]:
+
+
+
+
